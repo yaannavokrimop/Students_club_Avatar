5d0e752a1b686b06b3b6f68703a2ba718db98dd1
Anna <weinxor@gmail.com>
2020-11-30 05:04:50 +0300

    Добавление мест проведения

--- client/src/components/Dialogs/AddLocationDialog.vue
+++ client/src/components/Dialogs/AddLocationDialog.vue
@@ -6,64 +6,85 @@
                        v-on="on"
                        depressed block class="btn-light">
                     <v-icon class="mr-4">mdi-plus</v-icon>
-                    Добавить место проведения
+                    Добавить
                 </v-btn>
             </template>
             <StyledCard>
                 <template #title>Добавить место проведения</template>
                 <template #card-text>
-                    <v-container class="pb-0 pt-5">
-                        <v-row class="px-5">
-                            <v-col cols="4" class="py-0"><span>Адрес</span></v-col>
-                            <v-col cols="8" class="py-0">
-                                <v-text-field v-model="location.address" placeholder="Адрес" dense
-                                              outlined></v-text-field>
-                            </v-col>
-                        </v-row>
-                        <v-row v-if="askForData" class="px-5">
-                            <v-col cols="4" class="pt-2"><span>Дата</span></v-col>
-                            <v-col cols="4" class="py-0">
-                                <v-menu v-model="menuDate"
-                                        :close-on-content-click="false"
-                                        :nudge-right="40"
-                                        transition="scale-transition"
-                                        offset-y
-                                        min-width="290px">
-                                    <template v-slot:activator="{ on, attrs }">
-                                        <v-text-field v-model="location.date"
-                                                      placeholder="Дата"
-                                                      readonly
-                                                      outlined
-                                                      dense
-                                                      v-bind="attrs"
-                                                      v-on="on"></v-text-field>
-                                    </template>
-                                    <v-date-picker v-model="location.date"
-                                                   @input="menuDate = false"></v-date-picker>
-                                </v-menu>
-                            </v-col>
-                        </v-row>
-                        <v-row class="px-5">
-                            <v-col cols="4" class="pt-2"><span>Время начала и окончания</span></v-col>
-                            <v-col cols="4" class="py-0">
-                                <v-text-field outlined
-                                              dense
-                                              v-model="location.timeFrom"
-                                              type="time"></v-text-field>
-                            </v-col>
-                            <v-col cols="4" class="py-0">
-                                <v-text-field outlined
-                                              dense
-                                              v-model="location.timeTo"
-                                              type="time"></v-text-field>
-                            </v-col>
-                        </v-row>
-                    </v-container>
+                    <v-form ref="form">
+                        <v-container class="pb-0 pt-5">
+                            <v-row class="px-8">
+                                <v-switch v-model="itmo" label="Место проведения в ИТМО"></v-switch>
+                            </v-row>
+                            <v-row class="px-5" v-if="itmo">
+                                <v-col cols="4" class="py-0"><span>Корпус</span></v-col>
+                                <v-col cols="8" class="py-0">
+                                    <v-autocomplete v-model="location.building"
+                                                    :items="buildings"
+                                                    placeholder="Корпус" dense outlined></v-autocomplete>
+                                </v-col>
+                            </v-row>
+                            <v-row class="px-5" v-if="itmo">
+                                <v-col cols="4" class="py-0"><span>Аудитория</span></v-col>
+                                <v-col cols="8" class="py-0">
+                                    <v-autocomplete v-model="location.room"
+                                                    :items="rooms"
+                                                    placeholder="Аудитория" dense outlined></v-autocomplete>
+                                </v-col>
+                            </v-row>
+                            <v-row class="px-5" v-else>
+                                <v-col cols="4" class="py-0"><span>Адрес</span></v-col>
+                                <v-col cols="8" class="py-0">
+                                    <v-text-field v-model="location.address" placeholder="Адрес" dense
+                                                  outlined></v-text-field>
+                                </v-col>
+                            </v-row>
+                            <v-row v-if="askForData" class="px-5">
+                                <v-col cols="4" class="pt-2"><span>Дата</span></v-col>
+                                <v-col cols="4" class="py-0">
+                                    <v-menu v-model="menuDate"
+                                            :close-on-content-click="false"
+                                            :nudge-right="40"
+                                            transition="scale-transition"
+                                            offset-y
+                                            min-width="290px">
+                                        <template v-slot:activator="{ on, attrs }">
+                                            <v-text-field v-model="location.date"
+                                                          placeholder="Дата"
+                                                          readonly
+                                                          outlined
+                                                          dense
+                                                          v-bind="attrs"
+                                                          v-on="on"></v-text-field>
+                                        </template>
+                                        <v-date-picker v-model="location.date"
+                                                       @input="menuDate = false"></v-date-picker>
+                                    </v-menu>
+                                </v-col>
+                            </v-row>
+                            <v-row class="px-5">
+                                <v-col cols="4" class="pt-2"><span>Время начала и окончания</span></v-col>
+                                <v-col cols="4" class="py-0">
+                                    <v-text-field outlined
+                                                  dense
+                                                  v-model="location.timeFrom"
+                                                  type="time"></v-text-field>
+                                </v-col>
+                                <v-col cols="4" class="py-0">
+                                    <v-text-field outlined
+                                                  dense
+                                                  v-model="location.timeTo"
+                                                  type="time"></v-text-field>
+                                </v-col>
+                            </v-row>
+                        </v-container>
+                    </v-form>
                 </template>
                 <template #buttons>
                     <v-row class="mr-8 py-3">
                         <v-spacer></v-spacer>
-                        <v-btn depressed class="btn-accent">
+                        <v-btn @click="onAddLocation" depressed class="btn-accent">
                             Добавить
                         </v-btn>
                     </v-row>
@@ -84,13 +105,40 @@
         },
         data: () => ({
             dialog: false,
+            itmo: false,
             location: {
                 address: '',
+                building: '',
+                room: '',
                 date: '',
                 timeFrom: '',
                 timeTo: '',
             },
-        })
+            menuDate: false,
+            buildings: [
+                'Ломоносова',
+                'Кронверкский',
+                'Гривцова',
+                'Чайковского',
+            ],
+            rooms: [
+                'ITMO Place',
+                'Актовый зал',
+                'Коворкинг',
+                'Переговорная (268)',
+                'Переговорная в ректорском холле (254)',
+                'Ауд. 206',
+                'Ауд. 302',
+                'Ауд. 314'
+            ]
+        }),
+        methods: {
+            onAddLocation() {
+                this.$emit('addLocation', this.location);
+                this.dialog = false;
+                this.$refs.form.reset();
+            }
+        }
     }
 </script>
 
--- client/src/components/EditSection/Location.vue
+++ client/src/components/EditSection/Location.vue
@@ -7,14 +7,56 @@
                 </template>
                 <template #card-text>
                     <div class="pr-6 pl-3">
-                        <AddLocationDialog :askForData="multipleDays"/>
-                        <v-sheet v-if="locations.length" height="500">
-                            <v-calendar ref="calendar"
+                        <v-sheet height="450">
+                            <v-toolbar height="40" flat>
+                                <v-row>
+                                    <AddLocationDialog @addLocation="addLocation" :askForData="multipleDays"/>
+                                    <v-spacer></v-spacer>
+                                    <v-btn v-if="locations.length" fab text small color="grey darken-2" @click="prev">
+                                        <v-icon small>
+                                            mdi-chevron-left
+                                        </v-icon>
+                                    </v-btn>
+                                    <v-btn v-if="locations.length" fab text small color="grey darken-2" @click="next" class="mr-3">
+                                        <v-icon small>
+                                            mdi-chevron-right
+                                        </v-icon>
+                                    </v-btn>
+                                    <v-menu v-if="locations.length" bottom right >
+                                        <template v-slot:activator="{ on, attrs }">
+                                            <v-btn class="btn-light mr-5"
+                                                   depressed
+                                                   v-bind="attrs"
+                                                   v-on="on" >
+                                                <span>{{ typeToLabel[type] }}</span>
+                                                <v-icon right>
+                                                    mdi-menu-down
+                                                </v-icon>
+                                            </v-btn>
+                                        </template>
+                                        <v-list>
+                                            <v-list-item @click="changeCalenderType('category')">
+                                                <v-list-item-title>Один день</v-list-item-title>
+                                            </v-list-item>
+                                            <v-list-item @click="changeCalenderType('week')">
+                                                <v-list-item-title>Неделя</v-list-item-title>
+                                            </v-list-item>
+                                            <v-list-item @click="changeCalenderType('month')">
+                                                <v-list-item-title>Месяц</v-list-item-title>
+                                            </v-list-item>
+                                            <v-list-item @click="changeCalenderType('4day')">
+                                                <v-list-item-title>Четыре дня</v-list-item-title>
+                                            </v-list-item>
+                                        </v-list>
+                                    </v-menu>
+                                </v-row>
+                            </v-toolbar>
+                            <v-calendar v-if="locations.length"
+                                        ref="calendar"
                                         v-model="focus"
                                         color="primary"
-                                        type="category"
-                                        category-show-all
-                                        :events="locations"
+                                        :type="type"
+                                        :events="locationsCalendar"
                                         :start="today"
                                         :value="today"
                                         locale="ru"
@@ -42,6 +84,13 @@
         name: "Location",
         components: {StyledCard, AddLocationDialog},
         data: () => ({
+            type: 'category',
+            typeToLabel: {
+                month: 'Месяц',
+                week: 'Неделя',
+                category : 'Один день',
+                '4day': 'Четыре дня',
+            },
             menuDate: false,
             today: '2020-11-12',
             focus: '',
@@ -55,17 +104,51 @@
                     color: 'blue'
                 }*/
             ],
+            locationsCalendar: [],
             id: '',
             event: {}
         }),
         methods: {
             ...mapActions(['getEvent', 'editEvent']),
-            addLocation() {
-                
+            addLocation(location) {
+                const newLocation = {...location};
+                if (!this.multipleDays) {
+                    newLocation.date = this.event.dateFrom;
+                }
+
+                this.locations.push(newLocation);
+                this.parseLocation(newLocation)
+            },
+            parseLocation(item) {
+                let name;
+                if (item.address) {
+                    name = item.address
+                }
+                else if (this.type !== 'category') {
+                    name = item.building + ' ' + item.room;
+                }
+                else name = item.room;
+                this.locationsCalendar.push({
+                    name: name,
+                    start: item.date + ' ' + item.timeFrom,
+                    end: item.date + ' ' + item.timeTo,
+                    category: item.address ? item.address : item.building,
+                })
+            },
+            prev() {
+                this.$refs.calendar.prev()
+            },
+            next() {
+                this.$refs.calendar.next()
+            },
+            changeCalenderType(type) {
+                this.type = type;
+                this.locationsCalendar = [];
+                this.locations.forEach((item) => this.parseLocation(item));
             }
         },
         computed: {
-            multipleDays(){
+            multipleDays() {
                 return this.event.dateFrom !== this.event.dateTo;
             }
         },
@@ -86,6 +169,15 @@
 
 <style scoped>
     .theme--light.v-calendar-daily {
-        border: none
+        border: none;
+    }
+
+    ::v-deep .v-calendar-daily_head-day-label {
+        height: 40px;
+    }
+
+    ::v-deep .v-btn--fab.v-size--default {
+        height: 40px;
+        width: 40px;
     }
 </style>
